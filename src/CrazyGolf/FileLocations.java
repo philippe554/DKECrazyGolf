846a807fe5fd0c502a73bcd842e9f55fcfcec3db
MaximvsPrime <maximvanmil@live.nl>
2016-05-26 11:57:25 +0200

    anjkasdhfjlaskf

---
+++ src/CrazyGolf/FileLocations.java
@@ -0,0 +1,10 @@
+package CrazyGolf;
+
+/**
+ * Created by pmmde on 4/10/2016.
+ */
+public class FileLocations {
+    public static String openCLFile = "C:\\Users\\Maxim\\Downloads\\CrazyGolf1\\src\\CrazyGolf\\PhysicsEngine\\BallSide.cl";
+    public static String level1 = "C:\\Users\\Maxim\\Desktop\\field.txt";
+    public static String menuBG = "C:\\Users\\Maxim\\Downloads\\CrazyGolf1\\src\\CrazyGolf\\Menu\\game.jpg";
+}
