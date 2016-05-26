0d611d4864cb0f03d43885d10576cfa8e563804e
philippe554 <philippe.mmd@gmail.com>
2016-05-26 12:18:44 +0200

    update

---
+++ src/CrazyGolf/Bot/AngleBot/AngleBot.java
@@ -0,0 +1,76 @@
+package CrazyGolf.Bot.AngleBot;
+
+import CrazyGolf.Bot.BotInterface;
+import CrazyGolf.PhysicsEngine.Ball;
+import CrazyGolf.PhysicsEngine.World;
+import javafx.geometry.Point3D;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Created by Maxim on 24-5-2016.
+ */
+public class AngleBot implements BotInterface {
+
+    private World world;
+
+    @Override
+    public void init(World w) {
+        world = w;
+    }
+
+    @Override
+    public void calcNextShot(int playerNumber) {
+        ArrayList<Ball> ballArray = new ArrayList<Ball>();
+        Point3D direc = world.getHolePosition();
+        for(double i=10; i>0; i--){
+            for(double j=10;j>0; j--){
+                for(int h=0; h<=90;h++){
+
+                    Ball ball = new Ball(world.getBallSize(playerNumber), world.getBallPosition(playerNumber));
+                    ball.velocity = new Point3D(direc.getX() *0.1* i, direc.getY() + h , direc.getZ() *0.1*j);
+                    ballArray.add(ball);
+
+                    direc = new Point3D(direc.getX() * i, direc.getY() + h , direc.getZ() *j);
+
+                }
+            }
+        }
+        world.stepSimulated(ballArray, false);
+        Ball winner;
+        int bestDistanceBall = -1;
+        double bestDistance = -1;
+        for(int i = 0; i<ballArray.size()-1; i++){
+            Ball tempBall = ballArray.get(i);
+            //if(world.getHolePosition().distance(tempBall.place) < (tempBall.size)){
+            //    winner = tempBall;
+            //    world.pushBall(playerNumber, winner.velocity);
+            //}
+            //else{
+            double distance = Math.sqrt(Math.sqrt(world.getHolePosition().getX()-tempBall.place.getX()) + Math.sqrt(world.getHolePosition().getY()-tempBall.place.getY()));
+            if(bestDistance<distance){
+
+                bestDistanceBall = i;
+                //}
+            }
+            world.pushBall(playerNumber, ballArray.get(bestDistanceBall).velocity);
+        }
+
+    }
+
+    @Override
+    public void makeDatabase() {
+
+    }
+
+    @Override
+    public LinkedList<String> ouputDatabase() {
+        return null;
+    }
+
+    @Override
+    public void loadDatabase(LinkedList<String> input) {
+
+    }
+}
---
+++ src/CrazyGolf/Bot/RandomBot/RandomBot.java
@@ -0,0 +1,48 @@
+package CrazyGolf.Bot.RandomBot;
+
+import CrazyGolf.Bot.BotInterface;
+import CrazyGolf.PhysicsEngine.World;
+import javafx.geometry.Point3D;
+
+import java.util.LinkedList;
+
+import java.lang.Math;
+
+import static java.lang.Math.random;
+
+/**
+ * Created by Maxim on 26-5-2016.
+ */
+public class RandomBot implements BotInterface {
+
+    World world;
+    double scalar =40;
+
+    @Override
+    public void init(World w) {
+        world = w;
+    }
+
+    @Override
+    public void calcNextShot(int playerNumber) {
+        double i = Math.random() * scalar;
+        double j = Math.random() * scalar;
+        Point3D randomShot = new Point3D(i,j,0);
+        world.pushBall(playerNumber, randomShot);
+    }
+
+    @Override
+    public void makeDatabase() {
+
+    }
+
+    @Override
+    public LinkedList<String> ouputDatabase() {
+        return null;
+    }
+
+    @Override
+    public void loadDatabase(LinkedList<String> input) {
+
+    }
+}
